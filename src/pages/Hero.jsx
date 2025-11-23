0f1544d9b07b58382b07080ee14ab87a8f169eaf
𝑵𝒂𝒉𝒊𝒅 𝑨𝒉𝒎𝒆𝒅 <marupahmed92@gmail.com>
2025-11-23 21:20:56 +0600

    Update image URL for premium photo in Hero component

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -49,7 +49,7 @@ const Hero = () => {
         },
         {
             id: 5,
-            img: "https://images.unsplash.com/photo-1494336934272-f0efcedfc71a?q=80&w=800&auto=format&fit=crop", 
+            img: "https://plus.unsplash.com/premium_photo-1683121484963-a491b935780b?q=80&w=1291&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", 
             topText: "Premium",
             bottomLabel: "Quality",
             bottomValue: "100% Fresh",
